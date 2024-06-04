8a47510aab5613e4356b10e42473da856370c5b6
Keity <keityortegam@gmail.com>
2024-06-04 07:32:36 -0500

    Creación de carpetas con arquitectura hexagonal y creación de la entidad User

---
+++ Surveys_Riwi/src/main/java/com/riwi/Surveys_Riwi/utils/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.riwi.Surveys_Riwi.utils.exception;
+
+public class BadRequestException extends RuntimeException {
+    
+        public BadRequestException(String message){
+            super(message);
+        }
+}
